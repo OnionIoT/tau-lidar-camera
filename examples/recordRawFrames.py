cd270ef025f3dfa1ceb7c85ed7675977f93e4aa2
Lazar <lazardemin@gmail.com>
2021-03-18 16:05:33 -0400

    record frame example: added sigint (keyboard interrupt) handler for a cleaner looking exit

--- examples/recordRawFrames.py
+++ examples/recordRawFrames.py
@@ -1,11 +1,13 @@
 import os
 import time
+from signal import signal, SIGINT
 
 from TauLidarCommon.frame import FrameType
 from TauLidarCamera.camera import Camera
 
 
 outputDire = './samples'
+runLoop = True
 
 def setup():
     camera = None
@@ -15,7 +17,7 @@ def setup():
         camera = Camera.open(ports[0])             ## Open the first available Tau Camera
         camera.setModulationChannel(0)             ## autoChannelEnabled: 0, channel: 0
         camera.setIntegrationTime3d(0, 1000)       ## set integration time 0: 1000
-        camera.setMinimalAmplitude(0, 80)          ## set minimal amplitude 0: 80
+        camera.setMinimalAmplitude(0, 10)          ## set minimal amplitude 0: 80
 
         cameraInfo = camera.info()
 
@@ -27,25 +29,25 @@ def setup():
         print("    port:       %s" % cameraInfo.port)
 
         print("\nPress Esc key over GUI or Ctrl-c in terminal to shutdown ...")
-        
+
     return camera
 
 
 def run(camera):
-
+    global runLoop
     count = 0
 
     if not os.path.exists(outputDire):
         os.makedirs(outputDire)
 
     print('Recording...')
-    
-    while True:
+
+    while runLoop:
         frame = camera.readFrameRawData(FrameType.DISTANCE_AMPLITUDE)
 
         if frame:
             fName = '%s/%s.frame'%(outputDire, time.time())
-            with open(fName, "wb") as binary_file: 
+            with open(fName, "wb") as binary_file:
                 binary_file.write(frame)
             print('\rFrame: %d'%count, end='')
             count += 1
@@ -54,9 +56,14 @@ def cleanup(camera):
     print('\nShutting down ...')
     camera.close()
 
+def handler(signal_received, frame):
+    global runLoop
+    runLoop = False
+
 
 if __name__ == "__main__":
     camera = setup()
+    signal(SIGINT, handler)
 
     if camera:
         try:
